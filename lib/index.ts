b2081e74dcc81769a406d68bb3249ceb361fc49c
Vampo <umangboss786@gmail.com>
2023-06-21 00:53:25 +0530

    fix: latest

--- lib/index.ts
+++ lib/index.ts
@@ -2,8 +2,8 @@ import { CandyPay } from "@candypay/checkout-sdk";
 
 const candypay = new CandyPay({
   api_keys: {
-    public_api_key: "cp_public_3AJwPHcS_8LcAt5aueUJtVeFKjHHsEUos",
-    private_api_key: "cp_private_9wdoencK_4gTXbFVHH2U1qCo14DkUyCSC",
+    public_api_key: process.env[`NEXT_PUBLIC_CP_API`] as string,
+    private_api_key: process.env[`CANDYPAY_PRIVATE_KEY`] as string,
   },
   network: "mainnet",
   config: {
